bbb07105d53083a747118d95cb59469024c98ce6
mufteev.ia <imufteev@ya.ru>
2021-10-27 17:34:59 +0500

    init

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  switch (req.method) {
+    case 'GET':
+      try {
+        const { id = null, record = null } = req.query;
+        // const timestamp = new Date();
+
+        const t = `
+          ok - ${id}
+        `;
+
+        return res.end(t);
+      } catch (error) {
+        return res.end('bad'); 
+      }
+    default:
+      return res.end('missed');
+  }
 }
